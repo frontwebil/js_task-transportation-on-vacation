f16d8a2607c6b14877b1e1fee6982fb69e332faf
iLyhaha <gollpfd@gmail.com>
2026-02-28 19:37:18 +0200

    Refactoring Code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,18 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let total = days * 40;
+  const COST_PER_DAY = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM_DISCOUNT = 20;
+  const total = days * COST_PER_DAY;
 
   if (days > 6) {
-    total = total - 50;
-
-    return total;
+    return total - LONG_TERM_DISCOUNT;
   }
 
   if (days > 2) {
-    total = total - 20;
-
-    return total;
+    return total - MID_TERM_DISCOUNT;
   }
 
   return total;
